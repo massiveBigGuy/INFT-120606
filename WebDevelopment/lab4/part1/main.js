82bf984d75e5ea582cba4054067bd3bc3ff31197
massiveBigGuy <spencer.fevreau@dcmail.ca>
2024-10-30 16:19:42 -0400

    Create main.js
    Added info from raw-text from lab class to document.

---
+++ WebDevelopment/lab4/part1/main.js
@@ -0,0 +1,33 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+
+const insertx = ['Willy the Goblin', 'Big Daddy', 'Father Christmas']
+const inserty = ['the soup kitchen', 'Disneyland', 'the White House']
+const insertz = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away']
+
+randomize.addEventListener('click', result);
+
+function result() {
+    let newStory = storyText;
+
+    const xItem = randomValueFromArray(insertx);
+    const yItem = randomValueFromArray(inserty);
+    const zItem = randomValueFromArray(insertz);
+  if(customName.value !== '') {
+    const name = customName.value;
+  }
+
+  if(document.getElementById("uk").checked) {
+
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
