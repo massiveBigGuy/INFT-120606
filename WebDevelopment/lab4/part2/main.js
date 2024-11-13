139092b69f13cecc7c38dedde7ee0b98ec297b21
massiveBigGuy <spencer.fevreau@dcmail.ca>
2024-11-13 14:20:47 -0500

    Update main.js
    Removed unnecessary comments, added for loop to loop thru images.

--- WebDevelopment/lab4/part2/main.js
+++ WebDevelopment/lab4/part2/main.js
@@ -9,7 +9,6 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 const alts = {
   'pic1.jpg' : 'Closeup of a human eye',
@@ -19,7 +18,14 @@ const alts = {
   'pic5.jpg' : 'Large moth on a leaf'
 }
 
-/* Declaring the alternative text for each image file */
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
 
-/* Looping through images */
-/* Wiring up the Darken/Lighten button */
