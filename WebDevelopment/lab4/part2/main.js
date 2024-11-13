e8107d2015998f443db86456cc57e4e3db593df5
massiveBigGuy <spencer.fevreau@dcmail.ca>
2024-11-13 15:16:08 -0500

    Update main.js
    Adding button event listener, turns image dark when clicked.

--- WebDevelopment/lab4/part2/main.js
+++ WebDevelopment/lab4/part2/main.js
@@ -29,3 +29,15 @@ for (const image of images) {
   });
 }
 
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
