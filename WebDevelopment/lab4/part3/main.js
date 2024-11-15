d4c43ac91edb26ec197db27b82633c3793ff5776
massiveBigGuy <spencer.fevreau@dcmail.ca>
2024-11-15 14:39:59 -0500

    Update main.js
    Did final commenting for script

--- WebDevelopment/lab4/part3/main.js
+++ WebDevelopment/lab4/part3/main.js
@@ -3,7 +3,7 @@
 // Date: 13 November 2024
 // main.js for part 3 lab 4 - Web Development Fundamentals INFT 1206-06
 
-// setup canvas
+// Declare constants
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -11,18 +11,17 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-// function to generate random number
-
+// Function to generate random number
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random color
-
+// Function to generate random color
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
+// Class which contains rules for ball
 class Ball {
   constructor(x, y, velX, velY, color, size) {
     this.x = x;
@@ -32,12 +31,14 @@ class Ball {
     this.color = color;
     this.size = size;
   }
+  // Draw function to draw the ball
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
   }
+  // Update function which updates the ball's location with new information to keep the ball on canvas.
   update() {
     if ((this.x + this.size) >= width) {
       this.velX = -(this.velX);
@@ -58,6 +59,7 @@ class Ball {
     this.x += this.velX;
     this.y += this.velY;
   }
+  // Detect ball collisions to change colour when collision between balls happens.
   collisionDetect() {
     for (const ball of balls) {
       if (this !== ball) {
@@ -75,12 +77,11 @@ class Ball {
 }
 
 const balls = [];
-
+// Generates new balls when less than 25 balls on screen
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
+    // Ball position always drawn at least one ball width away from the edge of the canvas, to avoid drawing errors
     random(0 + size, width - size),
     random(0 + size, height - size),
     random(-7, 7),
@@ -92,6 +93,7 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+// Draw balls loop
 function loop() {
   ctx.fillStyle = "rgb(0 0 0 / 25%)";
   ctx.fillRect(0, 0, width, height);
@@ -105,4 +107,5 @@ function loop() {
   requestAnimationFrame(loop);
 }
 
+// Begin the ball loop
 loop();
