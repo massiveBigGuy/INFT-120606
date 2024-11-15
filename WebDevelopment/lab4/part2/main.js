fb72728e9db8c725ee6c76a0f3ea55b440e4602f
massiveBigGuy <spencer.fevreau@dcmail.ca>
2024-11-15 14:16:16 -0500

    Modified main.js
    Commented document and modified formatting.

--- WebDevelopment/lab4/part2/main.js
+++ WebDevelopment/lab4/part2/main.js
@@ -3,6 +3,7 @@
 // Date: 13 November 2024
 // main.js for part 2 lab 4 - Web Development Fundamentals INFT 1206-06
 
+// Declare Constants
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -18,6 +19,7 @@ const alts = {
   'pic5.jpg' : 'Large moth on a leaf'
 }
 
+// For loop which loops thru images in gallery
 for (const image of images) {
   const newImage = document.createElement('img');
   newImage.setAttribute('src', `images/${image}`);
@@ -29,13 +31,17 @@ for (const image of images) {
   });
 }
 
+// Event listener which decides to change btn to lighten or darken
 btn.addEventListener('click', () => {
   const btnClass = btn.getAttribute('class');
+  // If the btn constant has the class "dark" as assigned by default in the HTML, it darkens the image.
   if (btnClass === 'dark') {
     btn.setAttribute('class','light');
     btn.textContent = 'Lighten';
     overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-  } else {
+  }
+  // If the btn constant has a different class than "dark", changes it back to light mode.
+  else {
     btn.setAttribute('class','dark');
     btn.textContent = 'Darken';
     overlay.style.backgroundColor = 'rgba(0,0,0,0)';
