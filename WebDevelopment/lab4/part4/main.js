153f211c06653fbeb9a7769e283dc0f0c18f3e2f
massiveBigGuy <spencer.fevreau@dcmail.ca>
2024-11-22 23:53:12 -0500

    Updated all files
    Implemented EvilCircle object, added counter elements to style.css and index.html

--- WebDevelopment/lab4/part4/main.js
+++ WebDevelopment/lab4/part4/main.js
@@ -107,6 +107,50 @@ class EvilCircle extends Shape {
       }
     });
   }
+
+  // Draw the evil ball
+  draw() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  // Keep ball in bounds
+  checkBounds() {
+    if ((this.x + this.size) >= width) {
+      this.x -= this.size;
+    }
+
+    if ((this.x - this.size) <= 0) {
+      this.x += this.size;
+    }
+
+    if ((this.y + this.size) >= height) {
+      this.y -= this.size;
+    }
+
+    if ((this.y - this.size) <= 0) {
+      this.y += this.size;
+    }
+  }
+  // Detect collisions with balls, implement counter
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+          count--;
+          para.textContent = 'Ball count: ' + count;
+        }
+      }
+    }
+  }
 }
 
 const balls = [];
